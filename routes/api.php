4ace2ce8a8809ed887996867fccfc2c73c548600
RevenkoVladislav <Vlad_reven@mail.ru>
2025-10-23 23:37:17 +0600

    Добавлен тестовый api роут с посредником sanctum и проверена реализация через postman

--- routes/api.php
+++ routes/api.php
@@ -1,8 +1,14 @@
 <?php
 
+use App\Http\Controllers\GetController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/user', function (Request $request) {
     return $request->user();
+})->middleware('auth:sanctum');
+
+
+Route::group(['middleware' => 'auth:sanctum'], function () {
+   Route::get('/get', GetController::class);
 })->middleware('auth:sanctum');
