266a9aa9e358b2c9231b8c0fbb64dc4a3a11d483
RevenkoVladislav <Vlad_reven@mail.ru>
2025-10-24 17:40:09 +0600

    Подключение bootstrap

---
+++ resources/views/test.blade.php
@@ -0,0 +1,8 @@
+@vite(['resources/sass/app.scss', 'resources/js/app.js'])
+
+<div class="row">
+    <div class="row w-25">
+
+        <button class="btn btn-primary">Class</button>
+    </div>
+</div>
